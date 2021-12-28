df7f534c33136e763439cff5559aa132e4bc3169
Odelola <71991214+Odelola@users.noreply.github.com>
2021-12-29 00:18:08 +0100

    Add files via upload

---
+++ build/precache-manifest.49fba500462e480454cace8821b34719.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2849fdc2417bdf595c937db70baa66d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34ba76a794172ef2f8ef",
+    "url": "/static/css/2.3ab4c7df.chunk.css"
+  },
+  {
+    "revision": "6d9e18b81688c4ab87b7",
+    "url": "/static/css/main.db56f4b0.chunk.css"
+  },
+  {
+    "revision": "34ba76a794172ef2f8ef",
+    "url": "/static/js/2.8f503edf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8f503edf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d9e18b81688c4ab87b7",
+    "url": "/static/js/main.058d0eb4.chunk.js"
+  },
+  {
+    "revision": "bb5b94e5bf8a2ac4b1d8",
+    "url": "/static/js/runtime-main.6fd1fea2.js"
+  },
+  {
+    "revision": "62f10d46561ed0b911d42b7510d62d74",
+    "url": "/static/media/CrimsonText-Regular.62f10d46.ttf"
+  },
+  {
+    "revision": "8a743c206b0d21c667864106a9d97e89",
+    "url": "/static/media/ZillaSlab-Bold.8a743c20.ttf"
+  },
+  {
+    "revision": "15db01c5265adb04fd5e691e5ef26009",
+    "url": "/static/media/ZillaSlab-Regular.15db01c5.ttf"
+  },
+  {
+    "revision": "0d3a4e04c1008fc4179e35c9c55ade07",
+    "url": "/static/media/analytics.0d3a4e04.svg"
+  },
+  {
+    "revision": "282980d861150fce5f53f76a5ccf2a67",
+    "url": "/static/media/app.282980d8.png"
+  },
+  {
+    "revision": "befa7ad8ea4732694638a0b28e3af39d",
+    "url": "/static/media/app.befa7ad8.png"
+  },
+  {
+    "revision": "fd263a6b0b42266e2d84b8d5e810bb82",
+    "url": "/static/media/coin1.fd263a6b.png"
+  },
+  {
+    "revision": "932984809e22032a3f7c14b4a0cddcc6",
+    "url": "/static/media/coin2.93298480.png"
+  },
+  {
+    "revision": "431ab03915264216978110aaa1925436",
+    "url": "/static/media/coin3.431ab039.png"
+  },
+  {
+    "revision": "f5e20fd7484c37f10942cb78a6ab99f8",
+    "url": "/static/media/cube.f5e20fd7.png"
+  },
+  {
+    "revision": "8afd3780592b1a1e6869b18f7b6d0389",
+    "url": "/static/media/facebook.8afd3780.svg"
+  },
+  {
+    "revision": "58fa841d4accc404c93d154a28b98681",
+    "url": "/static/media/google.58fa841d.svg"
+  },
+  {
+    "revision": "69fd34c52c6238f5061c675141792aeb",
+    "url": "/static/media/helix.69fd34c5.png"
+  },
+  {
+    "revision": "d6e987e66982bd92b09833a00c33f904",
+    "url": "/static/media/news1.d6e987e6.png"
+  },
+  {
+    "revision": "f3e68cc09abeb99684bca857668835e1",
+    "url": "/static/media/news2.f3e68cc0.png"
+  },
+  {
+    "revision": "d82d597c3ab66e875d2b1d34564f21c0",
+    "url": "/static/media/pinterest.d82d597c.svg"
+  },
+  {
+    "revision": "588e21ea5a6d40a1aa925e79b909993f",
+    "url": "/static/media/pyramid.588e21ea.png"
+  },
+  {
+    "revision": "fe53943c4d96efddfa2b5f455adb1576",
+    "url": "/static/media/realtime.fe53943c.svg"
+  },
+  {
+    "revision": "ce35c56c68616470e5607c1d3ac93017",
+    "url": "/static/media/security.ce35c56c.svg"
+  },
+  {
+    "revision": "1cda7d52ec3d3381d6e5317264bd5e0d",
+    "url": "/static/media/sendandreceive.1cda7d52.svg"
+  },
+  {
+    "revision": "81aa7f2417f6a6f3b5bc9eb5ff91b5ab",
+    "url": "/static/media/sphere.81aa7f24.png"
+  },
+  {
+    "revision": "bdd8767e0d763df6ee480479ac85d48c",
+    "url": "/static/media/support.bdd8767e.svg"
+  },
+  {
+    "revision": "b6d1701868336600a333ea7bec662425",
+    "url": "/static/media/testimonials-1.b6d17018.jpg"
+  },
+  {
+    "revision": "08d8679b1e421ad63fd4d61156a5acd1",
+    "url": "/static/media/testimonials-2.08d8679b.jpg"
+  },
+  {
+    "revision": "3d677828b23ca5dc9653eb9e8f92aa1f",
+    "url": "/static/media/testimonials-3.3d677828.jpg"
+  },
+  {
+    "revision": "72fb04435870ed091240bd9da7474dab",
+    "url": "/static/media/testimonials-4.72fb0443.jpg"
+  },
+  {
+    "revision": "7e51704e0c22aa06e751f74e1ad70c75",
+    "url": "/static/media/twitch.7e51704e.svg"
+  },
+  {
+    "revision": "01792903f61e775b5fb753c7a1e36839",
+    "url": "/static/media/wallet.01792903.svg"
+  },
+  {
+    "revision": "a4faa3b5e91df4bcc9d71d73561ffb14",
+    "url": "/static/media/youtube.a4faa3b5.svg"
+  }
+]);
